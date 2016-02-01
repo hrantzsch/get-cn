afc0896f455c8be9419dd9594f179e93099ad610
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2016-02-01 14:52:24 +0100

    scraping one article

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import           Lib (getArticle)
 
-main :: IO ()
-main = someFunc
+-- main :: IO ()
+main = getArticle "http://news.sina.com.cn/c/nd/2016-01-28/doc-ifxnzanh0244522.shtml" "artibody"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,32 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Codec.Binary.UTF8.String (decodeString)
+import           Control.Monad
+import           Network.HTTP
+import           Text.HTML.TagSoup
+
+openURL :: String -> IO String
+openURL x = liftM decodeString $ getResponseBody =<< simpleHTTP (getRequest x)
+
+getArticleTags :: String -> String -> IO [Tag String]
+getArticleTags url articleId = do
+  src <- openURL url
+  return $ takeWhileNotClosed $
+           dropWhile (~/= TagOpen "div" [("id", articleId)]) (parseTags src)
+
+takeWhileNotClosed :: [Tag String] -> [Tag String]
+takeWhileNotClosed = findClosingTag 0
+  where
+    findClosingTag n (s:section)
+      | isTagOpen s = s : findClosingTag (n+1) section
+      | isTagClose s = if n <= 1 then [s]
+                                 else s : findClosingTag (n-1) section
+      | otherwise = s : findClosingTag n section
+
+splitTags :: [Tag String] -> [String]
+splitTags = undefined
+
+getArticle :: String -> String -> IO [String]
+getArticle url articleId = do
+  tags <- getArticleTags url articleId
+  return $ splitTags tags
