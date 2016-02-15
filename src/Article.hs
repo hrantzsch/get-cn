c6377f296e57ce44d5ce4025d12df07917f4cb97
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2016-02-15 18:10:23 +0100

    remove tagsoup from site.hs

--- src/Article.hs
+++ src/Article.hs
@@ -3,10 +3,8 @@
 module Article where
 
 import           Control.Exception
-import qualified Data.ByteString.Lazy.Internal as B
-import qualified Data.Text                     as T
-import           Network.HTTP.Conduit          (HttpException, simpleHttp)
-import           Text.HTML.DOM                 (parseLBS)
+import qualified Data.Text         as T
+import           Site              (cursorFor)
 import           Text.XML.Cursor
 
 scrapeArticle :: String -> String -> IO [String]
@@ -14,20 +12,6 @@ scrapeArticle targetId url = do
   c <- cursorFor url
   let artibody = c $// attributeIs "id" (T.pack targetId)
   return $ concatMap (splitParagraph . T.unpack) (head artibody $// content)
-
-cursorFor :: String -> IO Cursor
-cursorFor u = do
-     page <- try (simpleHttp u) :: IO (Either HttpException B.ByteString)
-     case page of
-       Left _  -> cursorFor $ sanitize u
-       Right p -> return (fromDocument $ parseLBS p)
-
-sanitize :: String -> String
--- just removes trailing space atm
-sanitize = reverse . maybeRmSpace . reverse
- where maybeRmSpace (x:xs) = case x of
-        ' ' -> xs
-        _   -> x:xs
 
 splitParagraph :: String -> [String]
 splitParagraph "" = []
--- src/Site.hs
+++ src/Site.hs
@@ -1,32 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Site where
 
-import           Codec.Binary.UTF8.String (decodeString)
 import           Control.Exception
-import           Control.Monad
-import           Data.List                as L
-import           Network.HTTP
-import           Network.Stream
-import           Text.HTML.TagSoup
-
-openURL :: String -> IO String
-openURL x = do
-  r <- try (simpleHTTP $ getRequest x) :: IO (Either SomeException (Result (Response String)))
-  case r of
-    Left _    -> openURL $ sanitize x
-    Right req -> liftM decodeString $ getResponseBody req
-
-getLinks :: [Tag String] -> [Tag String]
-getLinks = filter (~== TagOpen "a" [])
+import qualified Data.ByteString.Lazy.Internal as B
+import qualified Data.Text                     as T
+import           Network.HTTP.Conduit          (HttpException, simpleHttp)
+import           Text.HTML.DOM                 (parseLBS)
+import           Text.XML.Cursor
 
 getLinksMatching :: String -> (String -> Bool) -> IO [String]
 getLinksMatching url filterPattern = do
-  site <- openURL url
-  let links = map (fromAttrib "href") (getLinks $ parseTags site)
-  return $ filter filterPattern links
+  c <- cursorFor url
+  let links = c $// attribute "href"
+  return $ filter filterPattern $ map T.unpack links
+
+cursorFor :: String -> IO Cursor
+cursorFor url = do
+     page <- try (simpleHttp url) :: IO (Either HttpException B.ByteString)
+     case page of
+       Left _  -> cursorFor $ sanitize url
+       Right p -> return (fromDocument $ parseLBS p)
 
 sanitize :: String -> String
 -- just removes trailing space atm
 sanitize = reverse . maybeRmSpace . reverse
-  where maybeRmSpace (x:xs) = case x of
-          ' ' -> xs
-          _   -> x:xs
+ where maybeRmSpace (x:xs) = case x of
+        ' ' -> xs
+        _   -> x:xs
